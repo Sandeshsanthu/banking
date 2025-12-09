7c7944c42971e5c4e57ceae9bcd482cd8cc89488
Sandeshsanthu <41741287+Sandeshsanthu@users.noreply.github.com>
2025-12-09 12:34:08 +0530

    complete working code

--- internet-banking/privileges.sql
+++ internet-banking/privileges.sql
@@ -1,4 +1,7 @@
-CREATE USER 'javatodev_development'@'%' IDENTIFIED BY 'oPItyPticIAt';
+CREATE USER 'javatodev_development'@'%' IDENTIFIED WITH mysql_native_password BY 'oPItyPticIAt';
+GRANT ALL PRIVILEGES ON *.* TO 'javatodev_development'@'%';
+CREATE USER 'sandesh'@'%' IDENTIFIED WITH mysql_native_password BY 'sandesh';
+GRANT ALL PRIVILEGES ON *.* TO 'sandesh'@'%';
 GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES ON *.* TO 'sandesh'@'%';
 FLUSH PRIVILEGES;
 
